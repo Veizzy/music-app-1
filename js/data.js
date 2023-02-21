efb54b3cee33fa21800cc68fa964e1c2660ef8b9
Veizzy <109690019+Veizzy@users.noreply.github.com>
2023-02-21 21:58:48 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,120 @@
+
+let albums = [
+    {
+        title: `Пока крушишь кабинки в доте`,
+        img : `assets/1.jpg`,
+        year: `2023`,
+        description: `Самая нежная музыка`,
+        tracks: [
+            {
+                title: `Крутая музыка`,
+                author: `None`,
+                time: `3:07`,
+                src: `music/1.mp3`,
+                isPlaying: false,
+            },
+            {
+                title: `Прикольная музыка`,
+                author: `idk`,
+                time: `2:00`,
+                src: `music/2.mp3`,
+                isPlaying: false,
+            },
+            {
+                title: `Лютейшая музыка`,
+                author: `Вася`,
+                time: `3:08`,
+                src: `music/3.mp3`,
+                isPlaying: false,
+            }
+        ]
+    },
+    {
+        title: `Пока красишься`,
+        img : `assets/2.jpg`,
+        year: `2021`,
+        description: `Максимально расслабляющая музыка, перед которой ты не сможешь устоять`,
+        tracks: [
+            {
+                title: `Крутая музыка`,
+                author: `None`,
+                time: `3:07`,
+                src: `music/1.mp3`,
+                isPlaying: false,
+            },
+            {
+                title: `Да-да, музыка!`,
+                author: `idk`,
+                time: `2:00`,
+                src: `music/2.mp3`,
+                isPlaying: false,
+            },
+            {
+                title: `Музыка`,
+                author: `Вася`,
+                time: `3:08`,
+                src: `music/3.mp3`,
+                isPlaying: false,
+            }
+        ]
+    },
+    {
+        title: `Для милой прогулки`,
+        img : `assets/3.jpg`,
+        year: `2003`,
+        description: `Максимально расслабляющая музыка, перед которой ты не сможешь устоять`,
+        tracks: [
+            {
+                title: `Просто музыка`,
+                author: `None`,
+                time: `3:07`,
+                src: `music/1.mp3`,
+                isPlaying: false,
+            },
+            {
+                title: `Прикольная музыка`,
+                author: `idk`,
+                time: `2:00`,
+                src: `music/2.mp3`,
+                isPlaying: false,
+            },
+            {
+                title: `Афигеть, музыка`,
+                author: `Вася`,
+                time: `3:08`,
+                src: `music/3.mp3`,
+                isPlaying: false,
+            }
+        ]
+    },
+    {
+        title: `Собраться с мыслями`,
+        img : `assets/4.jpg`,
+        year: `2077`,
+        description: `Максимально расслабляющая музыка, перед которой ты не сможешь устоять`,
+        tracks: [
+            {
+                title: `Кайфовая музыка`,
+                author: `None`,
+                time: `3:07`,
+                src: `music/1.mp3`,
+                isPlaying: false,
+            },
+            {
+                title: `КРУТЕЙШАЯ музыка`,
+                author: `idk`,
+                time: `2:00`,
+                src: `music/2.mp3`,
+                isPlaying: false,
+            },
+            {
+                title: `Лютейшая музыка`,
+                author: `Вася`,
+                time: `3:08`,
+                src: `music/3.mp3`,
+                isPlaying: false,
+            }
+        ]
+        
+    },
+];
---
+++ js/index.js
@@ -0,0 +1,20 @@
+let container = document.querySelector(`.albums`);
+
+for(let i = 0; i < albums.length; i++) {
+    let album = albums[i];
+    container.innerHTML += `
+    <div class="albums my-1">
+                <div class="col">
+                    <a href="album.html?i=${i}" class="text-decoration-none">
+                    <div class="card">
+                        <img src="${album.img}" alt="" class="card-image-top">
+                        <div class="card-body">
+                            <p class="card-text">
+                                ${album.title}
+                            </p>
+                        </div>
+                    </div>
+                    </a>
+                </div>
+    `
+}
